862150b550676d3c203c6fa40cdcd48ee9da6e21
hritikk7 <kumarhritik158@gmail.com>
2024-10-14 18:16:41 +0530

    git user change

--- src/components/Steps.tsx
+++ src/components/Steps.tsx
@@ -22,7 +22,7 @@ export const Steps: React.FC = () => {
   const pathname = usePathname();
 
   return (
-    <ol className="lg:flex flex-col  rounded-md lg:rounded-none border-none  bg-white lg:border-l lg:border-gray-200  ">
+    <ol className="lg:flex flex-col  rounded-md lg:rounded-none  bg-white lg:border-l lg:border-gray-200  ">
       {STEPS.map((step, index) => {
         return <li></li>;
       })}
